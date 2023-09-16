e979a6fa40496f16e6e97301c8a10178e18765a7
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-09-15 23:56:33 -0700

    add login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,35 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport
-} from '@/components/ui/navigation-menu';
+import { Input } from '@/components/ui/input';
+import { useState, useEffect, FormEvent } from 'react';
 
 export default function Home() {
+  const [email, setEmail] = useState('mail@mail.com');
+  const [password, setPassword] = useState('password');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    console.log(email, password);
+    event.preventDefault();
+  };
+
   return (
     <>
-      Hello AI
-      <Button variant='destructive'>Button</Button>
+      <div className='w-80 mx-auto p-10 bg-slate-500'>
+        <h1>Sign in</h1>
+        <form onSubmit={handleSubmit}>
+          <Input
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <Input
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <Button variant='default'>Log in</Button>
+        </form>
+      </div>
     </>
   );
 }
