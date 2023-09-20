d218e90097d6b523168b0b0f44266bc067ec6d44
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-09-20 03:15:06 -0700

    add submit button to form

--- app/(dashboard)/(tool-routes)/tool-page.tsx
+++ app/(dashboard)/(tool-routes)/tool-page.tsx
@@ -1,18 +1,17 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { cn } from '@/lib/utils';
 import {
   ChangeEvent,
   Dispatch,
-  FormEvent,
   FormEventHandler,
   KeyboardEvent,
   SetStateAction,
-  useEffect,
-  useRef,
-  useState
+  useRef
 } from 'react';
+import { SendHorizonal } from 'lucide-react';
 
 const messagesHeight =
   'h-[calc(100vh-var(--message-box-height)-var(--navbar-height))]';
@@ -25,7 +24,7 @@ type Message = {
 
 const MessageContent = ({ messages }: { messages?: Message[] }) => {
   return (
-    <div className='w-full '>
+    <div className='w-full'>
       {messages?.map((message) => {
         const bgColor = message.isUserMessage && 'bg-slate-800';
         return (
@@ -59,12 +58,16 @@ export default function ToolPage({
 }) {
   const formRef = useRef<HTMLFormElement>(null);
 
+  const requestSubmitForm = () => {
+    formRef?.current?.dispatchEvent(
+      new Event('submit', { cancelable: true, bubbles: true })
+    );
+  };
+
   const textAreaKeyPressHandler = (event: KeyboardEvent) => {
     if (event.key === 'Enter' && event.shiftKey == false) {
       event.preventDefault();
-      formRef?.current?.dispatchEvent(
-        new Event('submit', { cancelable: true, bubbles: true })
-      );
+      requestSubmitForm();
     }
   };
 
@@ -75,7 +78,9 @@ export default function ToolPage({
 
   return (
     <div className='flex flex-col h-inherit w-full text-gray-200'>
-      <div className={cn('overflow-auto flex flex-col-reverse', messagesHeight)}>
+      <div
+        className={cn('overflow-auto flex flex-col-reverse', messagesHeight)}
+      >
         <MessageContent messages={messages} />
       </div>
       <form
@@ -83,13 +88,27 @@ export default function ToolPage({
         onSubmit={submitHandler}
         ref={formRef}
       >
-        <Textarea
-          value={inputMessage}
-          className='bg-slate-800 text-gray-200 w-full max-w-[800px]'
-          placeholder='Ask me anything'
-          onChange={textAreaChangeHandler}
-          onKeyDown={textAreaKeyPressHandler}
-        />
+        <div className='w-full relative max-w-[800px]'>
+          <Textarea
+            value={inputMessage}
+            className='bg-slate-800 text-gray-200 pr-[50px] outline-none'
+            placeholder='Ask me anything'
+            onChange={textAreaChangeHandler}
+            onKeyDown={textAreaKeyPressHandler}
+          />
+          <Button
+            className={cn(
+              'h-[40px] w-[40px] p-0 absolute bottom-4 right-4 bg-transparent transition-colors duration-500',
+              inputMessage.length > 0 && 'bg-green-600'
+            )}
+            onClick={(event) => {
+              event.preventDefault();
+              requestSubmitForm();
+            }}
+          >
+            <SendHorizonal size='20' />
+          </Button>
+        </div>
       </form>
     </div>
   );
