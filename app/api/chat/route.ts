39fb0675bfe2a95f1d278aa7a08a5d492fc23036
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-09-20 16:55:18 -0700

    fix import syntax error

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
 import { getCurrentUser } from "@/actions/auth";
-import openAI, { OpenAIError } from "@/lib/openai";
+import openAI from "@/lib/openai";
 import { NextRequest, NextResponse } from "next/server";
 import { APIError } from "openai";
 
